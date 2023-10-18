f7d56df67d2c41ac331f549cd79dc07b1d09b19f
Pradeep Marasini <prodeepmarasini@gmail.com>
2023-10-19 00:13:26 +0545

    current date

--- kit-date-converter.js
+++ kit-date-converter.js
@@ -31,8 +31,10 @@ function adtoBS(){
 
 
 
-const currentDateAD = new Date();
-const formattedDateAD = currentDateAD.toLocaleString('en-US', {
+
+const currentDateAD = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kathmandu' });
+
+const formattedDateAD = new Date(currentDateAD).toLocaleString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
@@ -41,21 +43,26 @@ const formattedDateAD = currentDateAD.toLocaleString('en-US', {
 document.getElementById('current-ad-date').innerText = `${formattedDateAD}`;
 document.querySelector('.result-date').innerText = `${formattedDateAD}`;
 
-let currentEnYear=currentDateAD.getFullYear();
-let currentEnMonth=currentDateAD.getMonth()+1;
-let currentEnDay=currentDateAD.getDate();
+let currentEnYear = new Date(currentDateAD).getFullYear();
+let currentEnMonth = new Date(currentDateAD).getMonth() + 1;
+let currentEnDay = new Date(currentDateAD).getDate();
 document.querySelector('.ad-year').value = currentEnYear;
 document.querySelector(`select.ad-month option[value="${currentEnMonth.toString().padStart(2, '0')}"]`).selected = true;
 document.querySelector('.ad-day').value = currentEnDay;
 
-const formattedDateADString = currentDateAD.toISOString().split('T')[0].replace(/-/g, '/');
-let formattedDateBS = new NepaliDate(new Date(formattedDateADString))
-document.getElementById('current-bs-date').innerText = `${formattedDateBS}`;
-document.querySelector('.bs-result-date').innerText = `${formattedDateBS}`;
 
-let currentNpYear=formattedDateBS.getYear();
-let currentNpMonth=formattedDateBS.getMonth()+1;
-let currentNpDay=formattedDateBS.getDate();
+const utcDate = new Date();
+utcDate.setUTCHours(utcDate.getUTCHours() + 5);
+utcDate.setUTCMinutes(utcDate.getUTCMinutes() + 45);
+const nepalTimezone = 'Asia/Kathmandu';
+const nepalDate = new Date(utcDate.toLocaleString('en-US', { timeZone: nepalTimezone }));
+const nepaliDate = new NepaliDate(new Date(nepalDate));
+document.getElementById('current-bs-date').innerText = `${nepaliDate}`;
+document.querySelector('.bs-result-date').innerText = `${nepaliDate}`;
+
+let currentNpYear = nepaliDate.getYear();
+let currentNpMonth = nepaliDate.getMonth() + 1;
+let currentNpDay = nepaliDate.getDate();
 document.querySelector('.bs-year').value = currentNpYear;
 document.querySelector(`select.bs-month option[value="${currentNpMonth.toString().padStart(2, '0')}"]`).selected = true;
 document.querySelector('.bs-day').value = currentNpDay;
